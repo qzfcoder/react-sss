8026d0b013df7cef001e71750ac2632c28b64627
qzfcoder <867303253@qq.com>
2022-09-28 16:50:39 +0800

    x

--- newnew—study-react/08_react_redux/src/app.jsx
+++ newnew—study-react/08_react_redux/src/app.jsx
@@ -1,38 +1,38 @@
-import React, { PureComponent } from 'react'
-import Profile from './pages/Profile'
-import Home from './pages/Home'
-import About from './pages/About'
-import './style.css'
-import store from './store'
+import React, { PureComponent } from "react";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import "./style.css";
+import store from "./store";
 export class app extends PureComponent {
   constructor() {
-    super()
+    super();
     this.state = {
-      counter: store.getState().counter
-    }
+      counter: store.getState().counter,
+    };
   }
   componentDidMount() {
     store.subscribe(() => {
-      const state = store.getState()
+      const state = store.getState();
       this.setState({
-        counter: state.counter
-      })
-    })
+        counter: state.counter,
+      });
+    });
   }
   render() {
-    const { counter } = this.state
+    const { counter } = this.state;
 
     return (
-      <div >
+      <div>
         <h2>App Counter：{counter} </h2>
         <div className="pages">
           <Home></Home>
           <Profile></Profile>
+          <About></About>
         </div>
-        <About></About>
-      </div >
-    )
+      </div>
+    );
   }
 }
 
-export default app
+export default app;
--- newnew—study-react/08_react_redux/src/pages/About.jsx
+++ newnew—study-react/08_react_redux/src/pages/About.jsx
@@ -1,16 +1,29 @@
-import React, { PureComponent } from 'react'
+import React, { PureComponent } from "react";
 // import store from '../store'
-import { connect } from 'react-redux'
-
+import { connect } from "react-redux";
+import store from "../store";
+import { addNumberAction, delNumberAction } from "../store/actionCreator";
 export class About extends PureComponent {
-
+  calcNumber(e, idADD) {
+    if (idADD) {
+      // store.dispatch(addNumberAction(e));
+      this.props.addNumber(e);
+    } else {
+      store.dispatch(delNumberAction(e));
+    }
+  }
   render() {
-    const { counter } = this.props
+    const { counter } = this.props;
     return (
       <div>
-        About:{counter}
+        <h2>About counter : {counter}</h2>
+        <div>
+          <button onClick={(e) => this.calcNumber(22, true)}>+22</button>
+          <button onClick={(e) => this.calcNumber(33, false)}>-33</button>
+          <button onClick={(e) => this.calcNumber(11, true)}>+11</button>
+        </div>
       </div>
-    )
+    );
   }
 }
 
@@ -18,8 +31,15 @@ function map(state) {
   return {
     counter: state.counter,
     // banner: state.banner
-  }
-
+  };
 }
-
-export default connect(map)(About)
+// 接受的是dispatch
+const fn2 = (dispatch) => {
+  return {
+    addNumber(num) {
+      dispatch(addNumberAction(num));
+    },
+    subNumber() {},
+  };
+};
+export default connect(map, fn2)(About);
